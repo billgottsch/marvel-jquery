cf822ab4d8dd467893e00b16b429606f1a523120
Bill Gottsch <billgottsch@gmail.com>
2016-11-20 09:56:57 -0600

    profile updates

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ if (heroReq == '' || heroReq == null || heroReq == undefined) {
      var group = $('<div class="groups"></div>');
      $.each(char.data.results, function(req, res){
         var subgroup = $('<div class="subgroup"></div>')
+        subgroup.append('<br>' + 'Name: ' + res.name);
         if (res.thumbnail.path === 'self' || res.thumbnail.path === null) {
           subgroup.append($('<br>' + '<img class="thumbplace">').attr('src', 'assets/reinhardt.jpg'));
         } else {
         subgroup.append($('<br>' + '<img class="thumbs">').attr('src', res.thumbnail.path + '.jpg'));
         };
-        subgroup.append('<br>' + 'Name: ' + res.name);
         subgroup.append('<br>' + 'Character ID: ' + res.id);
         subgroup.append('<br>' + '# of Comics: ' + res.comics.available);
         subgroup.append('<br>' + '# of Stories: ' + res.stories.available);
@@ -24,58 +24,9 @@ if (heroReq == '' || heroReq == null || heroReq == undefined) {
         subgroup.append('<br>' + '<a class="urlLink"></a>' + res.resourceURI).attr('href', res.resourceURI);
         group.append(subgroup)
     })
-    $('.marvel').empty().append(subgroup);
+    $('.marvel').empty().append(group);
    });
 
 };
 }
 $('#heroSelect').on("click", loadIt);
-
-
-
-
-
-// unction loadIt(){
-// var pageReq = $('#pageReq').val();
-// if (pageReq == '' || pageReq == null || pageReq == undefined) {
-//   alert('Your search terms were not valid')
-// } else {
-// $.getJSON(
-//   "https://www.reddit.com/r/" + pageReq + ".json",
-//   function goGetIt(data)
-//   {
-//     var group = $('<div class="group"></div>')
-//     $.each(
-//       data.data.children.slice(0, 25),
-//       function (request, result) {
-//         // var thumbnail = $('<img/>')
-//         var subgroup = $('<div class="subgroup"></div>')
-//         if (result.data.thumbnail == 'self' || result.data.thumbnail == null){
-//           subgroup.append($('<img class="thumbnail"/>').attr('src','https://cdn1.iconfinder.com/data/icons/school-supplies-3/64/folder_denied_stop_not_found_deleted_error-128.png'));
-//         } else {
-//           subgroup.append($('<img class="thumbnail"/>').attr('src',result.data.thumbnail));
-//         }
-//         subgroup.append($('<br>') );
-//         // var url = $('<a href='#'></a>')
-//         subgroup.append($('<a class="title"></a>').attr('href',result.data.url).attr('target',"_blank").text(result.data.title));
-//         // var description = $('<p></p>')
-//         subgroup.append($('<p class="description"></p>').text("Click to read more!"));
-//         // // var ups = $('<p></p>')
-//         subgroup.append($('<div class="ups"></div>').text(result.data.ups) );
-//         // // var num_comments = $('<p></p>')
-//         subgroup.append($('<div class="comments"></div>').text(result.data.num_comments) );
-//         subgroup.append($('<hr>') );
-//         group.append(subgroup);
-//
-//       })
-//     $("#content").empty().append(group);
-//
-//   }).fail(function() {
-//     alert("Your search terms were not valid")
-// });
-// // .success(function() { alert("second success"); })
-// // .complete(function() { alert("complete"); });
-// }
-// }
-//
-// $('#loadReq').on("click", loadIt);
